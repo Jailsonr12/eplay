ab89365ca3134fb5e03ad52c763757ec181ee8ab
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2026-03-14 16:52:24 -0300

    prettier comitado

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,9 @@
-const Banner = () => <div>Banner</div>
+import { Image } from './style'
+
+import bannerImg from '../../assets/images/banner-homem-aranha.png'
+
+const Banner = () => (
+  <Image style={{ backgroundImage: `url(${bannerImg})` }}> banner </Image>
+)
 
 export default Banner
